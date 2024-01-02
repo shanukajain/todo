fcadf1638a679648ec0f3cdca3f38f09584609ec
ShanukaJain <jain.shanuka@gmail.com>
2024-01-02 16:35:39 +0530

    index file

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 const express=require("express");
 const { connection } = require("./config");
 const mongoose=require("mongoose");
-
 const { todorouter } = require("./route");
 const { userrouter } = require("./user");
 const { authantication } = require("./authantication");
 const app=express();
-app.use(express.json());
+
+// index file 
+
 require("dotenv").config();
 const port=process.env.port;
+
+
+app.use(express.json());
 app.use("/users",userrouter);
 app.use(authantication);
 app.use("/todo",todorouter);
+
+
+
 app.get("/",(req,res)=>{
     res.send("home page");
 })
+
+
+
+
 
 app.listen(port,async()=>{
     console.log(`port ${port}`);  
